5b292d0873290a65ede2307fcfc1b1d1787c31f7
Brian Berns <brianberns@gmail.com>
2020-05-07 02:48:09 -0400

    Added another example.

--- src/Eff.Harness/Program.fs
+++ src/Eff.Harness/Program.fs
@@ -1,46 +1,46 @@
 ﻿open Eff.Core
 
-let stateTest () =
+module StateTest =
 
-    let comp =
-        eff {
-            let! x = State.get ()
-            do! State.put (x + 1)
-            let! y = State.get ()
-            do! State.put (y + y)
-            return! State.get ()
-        } 
+    let run () =
 
-    let x = comp |> State.stateHandler 1 |> Effect.run
-    assert(x = (4, 4))
-    printfn "%A" x
+        let comp =
+            eff {
+                let! x = State.get ()
+                do! State.put (x + 1)
+                let! y = State.get ()
+                do! State.put (y + y)
+                return! State.get ()
+            } 
 
-let stackTest () =
+        let x = comp |> State.stateHandler 1 |> Effect.run
+        assert(x = (4, 4))
+        printfn "%A" x
 
-    let comp : Inc<Stack<int>, int> =
-        eff {
-            let! a = Stack.pop ()
-            if a = 5 then
-                do! Stack.push 7
-            else
-                do! Stack.push 3
-                do! Stack.push 8
-            return a
-        }
+module StackTest =
 
-    let stack = Eff.Collections.Stack.ofList [9; 0; 2; 1; 0]
-    let x = comp |> Stack.stackHandler stack |> Effect.run
-    assert(x = (Eff.Collections.Stack.ofList [8; 3; 0; 2; 1; 0], 9))
-    printfn "%A" x
+    let run () =
 
-type CombinedEffect =
-    inherit State<int>
-    inherit Reader<int>
+        let comp : Inc<Stack<int>, int> =
+            eff {
+                let! a = Stack.pop ()
+                if a = 5 then
+                    do! Stack.push 7
+                else
+                    do! Stack.push 3
+                    do! Stack.push 8
+                return a
+            }
+
+        let stack = Eff.Collections.Stack.ofList [9; 0; 2; 1; 0]
+        let x = comp |> Stack.stackHandler stack |> Effect.run
+        assert(x = (Eff.Collections.Stack.ofList [8; 3; 0; 2; 1; 0], 9))
+        printfn "%A" x
 
 /// Combine State and Reader effects.
-let combinedTest () =
+module StateReaderTest =
 
-    let comp =
+    let getComp () : Inc<'a,int> when 'a :> State<int> and 'a :> Reader<int> =
         eff {
             do! State.put 1
             let! y = Reader.ask ()
@@ -48,34 +48,73 @@ let combinedTest () =
             return x + y
         }
 
-    let x =
-        comp
-            |> State.stateHandler<CombinedEffect, _, _> 0
-            |> Reader.readerHandler 1
-            |> Effect.run
-    assert(x = (1, 2))
-    printfn "%A" x
+    type CombinedEffect =
+        inherit State<int>
+        inherit Reader<int>
 
-let nonDetTest () =
+    let run () =
 
-    let comp = 
+        let x =
+            getComp ()
+                |> State.stateHandler<CombinedEffect, _, _> 0
+                |> Reader.readerHandler 1
+                |> Effect.run
+        assert(x = (1, 2))
+        printfn "%A" x
+
+module NonDetTest =
+
+    let run () =
+
+        let comp = 
+            eff {
+                let! x = NonDet.choose (1, 2)
+                let! y = NonDet.choose ("1", "2")
+                return (x, y)
+            }
+
+        let x =
+            comp
+                |> NonDet.nonDetHandler
+                |> Effect.run
+        assert(x = [(1, "1"); (1, "2"); (2, "1"); (2, "2")])
+        printfn "%A" x
+
+module StateNonDetTest =
+
+    let getComp () =
         eff {
-            let! x = NonDet.choose (1, 2)
-            let! y = NonDet.choose ("1", "2")
+            let! n = State.get ()
+            let! x = NonDet.choose (n, n + 1)
+            let! y = NonDet.choose (string n, string (n + 1))
             return (x, y)
         }
 
-    let x =
-        comp
-            |> NonDet.nonDetHandler
-            |> Effect.run
-    assert(x = [(1, "1"); (1, "2"); (2, "1"); (2, "2")])
-    printfn "%A" x
+    type CombinedEffect =
+        inherit State<int>
+        inherit NonDetEffect
+
+    let run () =
+        let x =
+            getComp ()
+                |> State.stateHandler<CombinedEffect, _, _> 1
+                |> NonDet.nonDetHandler
+                |> Effect.run
+        assert(
+            x =
+                [
+                    1, (1, "1")
+                    1, (1, "2")
+                    1, (2, "1")
+                    1, (2, "2")
+                ])
+        printfn "%A" x
 
 [<EntryPoint>]
 let main argv =
-    stateTest ()
-    stackTest ()
-    combinedTest ()
-    nonDetTest ()
+    StateTest.run ()
+    StackTest.run ()
+    StateReaderTest.run ()
+    NonDetTest.run ()
+    StateNonDetTest.run ()
     0
