54032ed5ff9dcc93a843679b5ce16df31a390d33
Brian Berns <brianberns@gmail.com>
2020-05-07 02:00:07 -0400

    Added non-deterministic test.

--- src/Eff.Harness/Program.fs
+++ src/Eff.Harness/Program.fs
@@ -1,6 +1,6 @@
 ﻿open Eff.Core
 
-let testState () =
+let stateTest () =
 
     let comp =
         eff {
@@ -11,10 +11,11 @@ let testState () =
             return! State.get ()
         } 
 
-    let x = comp |> State.stateHandler 1 |> Effect.run // (4, 4)
+    let x = comp |> State.stateHandler 1 |> Effect.run
+    assert(x = (4, 4))
     printfn "%A" x
 
-let testStack () =
+let stackTest () =
 
     let comp : Inc<Stack<int>, int> =
         eff {
@@ -27,17 +28,17 @@ let testStack () =
             return a
         }
 
-    let stack = [9; 0; 2; 1; 0] |> Eff.Collections.Stack.ofList
-    let x = comp |> Stack.stackHandler stack |> Effect.run   // (Stack [8; 3; 0; 2; 1; 0], 9)
+    let stack = Eff.Collections.Stack.ofList [9; 0; 2; 1; 0]
+    let x = comp |> Stack.stackHandler stack |> Effect.run
+    assert(x = (Eff.Collections.Stack.ofList [8; 3; 0; 2; 1; 0], 9))
     printfn "%A" x
-
 
 type CombinedEffect =
     inherit State<int>
     inherit Reader<int>
 
 /// Combine State and Reader effects.
-let testCombined () =
+let combinedTest () =
 
     let comp =
         eff {
@@ -51,12 +52,30 @@ let testCombined () =
         comp
             |> State.stateHandler<CombinedEffect, _, _> 0
             |> Reader.readerHandler 1
-            |> Effect.run // (1, 2)
+            |> Effect.run
+    assert(x = (1, 2))
+    printfn "%A" x
+
+let nonDetTest () =
+
+    let comp = 
+        eff {
+            let! x = NonDet.choose (1, 2)
+            let! y = NonDet.choose ("1", "2")
+            return (x, y)
+        }
+
+    let x =
+        comp
+            |> NonDet.nonDetHandler
+            |> Effect.run
+    assert(x = [(1, "1"); (1, "2"); (2, "1"); (2, "2")])
     printfn "%A" x
 
 [<EntryPoint>]
 let main argv =
-    testState ()
-    testStack ()
-    testCombined ()
+    stateTest ()
+    stackTest ()
+    combinedTest ()
+    nonDetTest ()
     0
